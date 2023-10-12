b226d8bc03dfbc0bf5abd1ce241f99fb30c2b829
Solodkih <solodkih_v_p@mail.ru>
2023-10-12 14:54:20 +0600

    -create result function -create reducerResult

--- script-redux.js
+++ script-redux.js
@@ -1,12 +1,19 @@
 {
-  let state = [[]];
+  let state = {
+    result: null,
+    array: [[]],
+  };
   let listener;
 
   function dispatch(action) {
-    let newState = reducer(state, action);
-    if (newState !== state) {
+    let newState = {
+      result: reducerResult(state.result, action),
+      array: reducerArray(state.array, action),
+    };
+    if (newState.array !== state.array || newState.result !== state.result) {
       state = newState;
-      //listener();
+      console.log(newState);
+      listener(store.getState());
     }
   }
 
@@ -24,13 +31,75 @@
     subscribe,
   };
 
+  function divMulSubAdd(first, symbol, second) {
+    switch (symbol) {
+      case "*":
+        return first * second;
+
+      case "+":
+        return first + second;
+
+      case "-":
+        return first - second;
+
+      case "/":
+        return first / second;
+
+      default:
+        return 0;
+    }
+  }
+
+  function result(state) {
+    const copyState = state.map((item) => {
+      return item.map((item) => {
+        return item;
+      });
+    });
+
+    const arrayNumber = copyState.map((item) => {
+      if (!ArrayNotContainDivMulSubAdd(item)) {
+        return item.at(-1);
+      }
+      const newItem = item.reduce((str, item, index) => {
+        if (index === 0) {
+          str = "";
+        }
+        str += item;
+        return str;
+      });
+      return Number(newItem);
+    });
+
+    arrayNumber.forEach((item, index, array) => {
+      if (item !== "*" && item !== "/") {
+        return;
+      }
+      array[index + 1] = divMulSubAdd(array[index - 1], item, array[index + 1]);
+      array[index - 1] = null;
+      array[index] = null;
+    });
+
+    const arrayNumberWithoutDivMull = arrayNumber.filter((item) => item !== null);
+
+    arrayNumberWithoutDivMull.forEach((item, index, array) => {
+      if (item === "+" || item === "-") {
+        array[index + 1] = divMulSubAdd(array[index - 1], item, array[index + 1]);
+        array[index - 1] = null;
+        array[index] = null;
+      }
+    });
+
+    return arrayNumberWithoutDivMull.at(-1);
+  }
+
   function ArrayNotContainDivMulSubAdd(array) {
     return (
       array.at(-1) !== "*" && array.at(-1) !== "+" && array.at(-1) !== "-" && array.at(-1) !== "/"
     );
   }
 
-  function reducer(state, action) {
+  function reducerArray(state, action) {
     switch (action.type) {
       case "0":
       case "1":
@@ -126,13 +195,29 @@
           return state;
         })();
 
-      case "=":
-        return (() => {
-          return state;
-        })();
-
       default:
         return state;
     }
+  }
+}
+
+function reducerResult(state, action) {
+  switch (action.type) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "C":
+      return null;
+    case "=":
+      return result(store.getState().array);
+    default:
+      return state;
   }
 }
